2a386ea6b23021d887b921c81b029e28875010c9
coskun cakir <csknckr@gmail.com>
2020-12-21 17:43:38 +0000

    feat: language switcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,12 @@ import Layout from "../components/layout"
 import Container from "../components/container"
 import Title from "../components/title"
 import Button from "../components/button"
-import { useLocalization, LocalesList } from "gatsby-theme-i18n"
+import { useLocalization, LocalizedLink } from "gatsby-theme-i18n"
 import { useIntl } from "react-intl"
-import Language from "../components/language"
 
 export default function Home() {
   const intl = useIntl()
-
-  const { locale, config, defaultLang } = useLocalization()
+  const { locale, defaultLang } = useLocalization()
 
   return (
     <>
@@ -19,13 +17,14 @@ export default function Home() {
       <Layout>
         <Container>
           <Title headingLevel="h1">Hello world!</Title>
-          <Language />
           <Button>Hello</Button>
           <div>Current locale: {locale}</div>
           <div>Current defaultLang: {defaultLang}</div>
-          <pre>{JSON.stringify(config, null, 2)}</pre>
-          <LocalesList />
           {intl.formatMessage({ id: "home" })}
+          <br />
+          <LocalizedLink to="/about">
+            {intl.formatMessage({ id: "about" })}
+          </LocalizedLink>
         </Container>
       </Layout>
     </>
