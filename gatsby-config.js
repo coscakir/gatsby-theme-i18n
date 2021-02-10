9e68798a6077ffb77e3bb3a93701888671a0d0e6
coskun cakir <csknckr@gmail.com>
2021-02-10 14:13:26 +0000

    unnecessary files removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Boat Trip Turkey",
+    title: "Gatsbyjs i18n",
     titleTemplate: "%s",
     description:
-      "Gulet charter boat holidays. Day trips or longer. We focus exclusively on Gulet cruises that are tailored to our guests requirements. Kas, Kalkan and Kekova’s premier gulet cruise and blue cruise operator in the area.",
-    url: "https://boattripturkey.com",
+      "A multilingual Gatsbyjs project example with language switcher",
+    url: "https://gatsby-theme-i18n.netlify.app",
     image: "",
-    twitterUsername: "@captainergun",
-    siteUrl: `https://boattripturkey.com`,
+    twitterUsername: "@coscakir",
+    siteUrl: `https://gatsby-theme-i18n.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `BoatTripTurkey`,
-        short_name: `BoatTripTurkey`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#1F6BB6`,
-        display: `standalone`,
-        icon: `${__dirname}/static/assets/images/icon.png`,
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -31,13 +18,6 @@ module.exports = {
         path: `${__dirname}/static/assets/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaultQuality: 100,
-      },
-    },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-theme-i18n`,
